fd652cc6449bbb25a0e661a480f8df6407a61deb
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-03-30 18:16:02 +0900

    Fix bug in login functionality

---
+++ 정규표현식/염색체.py
@@ -0,0 +1,17 @@
+import re
+T = int(input())
+
+while T:
+    s = input()
+    if re.match(r'^([ABCDEF]?A+F+C+[ABCDEF]?$', s):
+        print('Infected!')
+    else:
+        print('Good')
+    T -= 1
+
+'''
+^으로 시작
+$으로 끝
+?는 0 또는 1회 반복
++는 1회 이상 반복
+'''
