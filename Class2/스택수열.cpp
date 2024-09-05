5b1f6f10e7d8280aaf50b8c0c0d7b0767579a001
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-09-05 11:00:55 +0900

    version 2

--- Class2/스택수열.cpp
+++ Class2/스택수열.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
-#include <list>
+#include <vector> // vector는 메모리를 연속적으로 할당하기 때문에 배열처럼 사용 가능, 메모리 사용량이 적음
+#include <algorithm> // for sort
 using namespace std;
 
-string possible(list<int> sortedL, list<int> rawL) {
-    list<int> s;
+string possible(vector<int>& sortedL, vector<int>& rawL) {
+    vector<int> s;
     string result = "";
-    while (!rawL.empty()) {
-        if (sortedL.front() <= rawL.front()) {
-            s.push_back(sortedL.front());
-            sortedL.pop_front();
+    size_t sortedIndex = 0;
+    size_t rawIndex = 0;
+
+    while (rawIndex < rawL.size()) {
+        if (sortedIndex < sortedL.size() && sortedL[sortedIndex] <= rawL[rawIndex]) {
+            s.push_back(sortedL[sortedIndex]);
+            sortedIndex++;
             result += "+";
         }
-        else if (s.back() == rawL.front()) {
+        else if (!s.empty() && s.back() == rawL[rawIndex]) {
             s.pop_back();
-            rawL.pop_front();
+            rawIndex++;
             result += "-";
         }
         else {
@@ -25,21 +29,29 @@ string possible(list<int> sortedL, list<int> rawL) {
 
 int main() {
     int n;
-    list<int> L;
-    list<int> rL;
+    vector<int> L;
     cin >> n;
+    L.reserve(n);
+
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
         L.push_back(x);
     }
-    rL = L;
-    L.sort();
 
-    
+    vector<int> rL = L;
+    sort(L.begin(), L.end());
 
-    for (char c : possible(L, rL)) {
-        cout << c << '\n';
+    string result = possible(L, rL);
+    for (size_t i = 0; i < result.size(); ++i) {
+        if (result[i] == 'N') {
+            cout << "NO";
+            return 0;
+        }
+        cout << result[i];
+        if (i != result.size() - 1) {
+            cout << '\n';
+        }
     }
     return 0;
 }
