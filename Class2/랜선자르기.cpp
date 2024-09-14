add587e3281f9b027580456f187029d3071fbce3
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-09-14 15:46:42 +0900

    Refactor code for binary search algorithm in "랜선자르기.cpp"

---
+++ Class2/랜선자르기.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long K, N; // K: 랜선의 개수, N: 필요한 랜선의 개수 (long으로 overflow 방지)
+long arr[10000];
+int res;
+
+void binary(long long low, long long high) {
+    if (low > high) {
+        return;
+    }
+    long long mid = (low + high) / 2;
+    long cnt = 0;
+    for (int i = 0; i < K; i++) {
+        if (arr[i] == 0) {
+            break; // 요소가 0이면 반복을 멈춤
+        }
+        cnt += arr[i] / mid; // 만들어지는 랜선의 개수
+    }
+    if (cnt >= N) {
+        res = mid;
+        binary(mid + 1, high);
+    }
+    else {
+        binary(low, mid - 1);
+    }   
+}
+
+int main() {
+    int length; // 랜선의 길이
+    long long low = 1, high = 0; // 이진 탐색을 위한 변수
+    cin >> K >> N;
+    for (int i = 0; i < K; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + K);
+    high = arr[K - 1];
+    binary(low, high + 1);
+    cout << res;
+    return 0;
+}
