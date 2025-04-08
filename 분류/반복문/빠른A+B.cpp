7b1cc3b6f74f82f10f24983e03e3efd4e5dc1cd8
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-04-08 09:27:11 +0900

    implement loop

---
+++ 분류/반복문/빠른A+B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int T;
+    cin >> T; // 테스트 케이스 수
+    while (T--)
+    {
+        int A, B;
+        cin >> A >> B;         // A와 B 입력
+        cout << A + B << '\n'; // A와 B의 합 출력
+    }
+}
