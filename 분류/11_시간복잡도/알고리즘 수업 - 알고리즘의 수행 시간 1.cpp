dc4697f858c416328f3d323ca9fd35abffad1cec
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-05-27 10:10:24 +0900

    time complexity analysis

---
+++ 분류/11_시간복잡도/알고리즘 수업 - 알고리즘의 수행 시간 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/*
+MenOfPassion(A[], n) {
+    int = [n/2];
+    return A[i]; # 코드1 -> 항상 1번 실행됨
+}
+
+*/
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n; // 입력의 크기
+    cin >> n;
+
+    cout << 1 << '\n'
+         << 0;
+}
