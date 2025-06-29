3ace5cd67a3233285af82d6632e3573124f9325c
HyeonJun, Lee <119414382+hyeon-BL@users.noreply.github.com>
2025-06-29 09:37:25 +0900

    The number of cases - 1 << N

---
+++ 분류/17_조합론/녹색거탑.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
+  int N;
+  cin >> N;
+  cout << pow(2, N) << '\n';
+  return 0;
+}
