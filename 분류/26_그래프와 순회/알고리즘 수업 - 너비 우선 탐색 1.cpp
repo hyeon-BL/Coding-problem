143ecd3993dd64726c3b8735795ca4b8ec22ef41
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-08-08 10:52:00 +0900

    Graph traversal - BFS 1

---
+++ 분류/26_그래프와 순회/알고리즘 수업 - 너비 우선 탐색 1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> adj[100'001];
+vector<int> visited;
+vector<int> order;
+
+void bfs(int R)
+{
+    queue<int> q;
+    q.push(R);
+    visited[R] = 1;
+
+    int cnt = 1;      // 방문 순서 카운트
+    order[R] = cnt++; // 방문 순서 기록
+
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+
+        for (int next : adj[curr])
+        {
+            if (!visited[next])
+            {
+                visited[next] = 1;
+                order[next] = cnt++; // 방문 순서 기록
+                q.push(next);
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int N, M, R;
+    cin >> N >> M >> R;
+
+    visited.resize(N + 1, 0);
+    order.resize(N + 1, 0);
+    for (int i = 0; i < M; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        sort(adj[i].begin(), adj[i].end());
+    }
+    bfs(R);
+
+    for (int i = 1; i <= N; i++)
+    {
+        cout << order[i] << "\n";
+    }
+
+    return 0;
+}
