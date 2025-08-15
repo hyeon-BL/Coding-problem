55c6cf23287d9e4816c21578c988f878ddf6544f
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-08-15 11:54:19 +0900

    Floyd-warshall algorithm - can find shortest cycle for all of the nodes

---
+++ 분류/28_최단 경로/운동.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+vector<int> dijkstra(int start, int n, const vector<vector<pair<int, int>>> &graph)
+{
+    vector<int> dist(n + 1, INT_MAX);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dist[start] = 0;
+    pq.push({0, start});
+
+    while (!pq.empty())
+    {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (d > dist[u])
+        {
+            continue;
+        }
+
+        for (const auto &edge : graph[u])
+        {
+            int v = edge.first;
+            int w = edge.second;
+
+            if (dist[u] + w < dist[v])
+            {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    return dist;
+}
+
+int shortest_cycle(int n, const vector<vector<pair<int, int>>> &graph)
+{
+    int min_cycle_length = INT_MAX;
+
+    vector<vector<int>> all_distances(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        vector<int> dist = dijkstra(i, n, graph);
+        all_distances[i] = dist;
+    }
+
+    for (int u = 1; u <= n; u++)
+    {
+        for (const auto &edge : graph[u])
+        {
+            int v = edge.first;
+            int w = edge.second;
+            if (all_distances[v][u] != INT_MAX)
+            {
+                min_cycle_length = min(min_cycle_length, all_distances[u][v] + all_distances[v][u]);
+            }
+        }
+    }
+
+    return min_cycle_length == INT_MAX ? -1 : min_cycle_length;
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<pair<int, int>>> graph(V + 1);
+    for (int i = 0; i < E; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+    }
+
+    int result = shortest_cycle(V, graph);
+    cout << result << "\n";
+}
