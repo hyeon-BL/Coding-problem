6622f0fe8995ed5face833bcb5f2c8413e10c070
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-10-06 13:09:30 +0900

    Interactive program - basic I/O

---
+++ 분류/37_인터랙티브와 투스탭/A+B-10(제1편).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int main()
+{
+    int resp;
+    for (int a = 1; a <= 9; a++)
+    {
+        // A가 a인지 물어보고 flush를 한다.
+        // endl은 자동으로 flush도 해준다.
+        std::cout << "? A " << a << std::endl;
+
+        // 채점기의 답변을 입력받는다.
+        std::cin >> resp;
+
+        if (resp == 1)
+        {
+            // 답변이 "예"이므로 A의 값은 a이다.
+            // B의 값도 알아내야 하는데, 이 부분은 직접 채워보자.
+            int b = 0;
+            for (int i = 1; i <= 9; i++)
+            {
+                std::cout << "? B " << i << std::endl;
+                std::cin >> resp;
+                if (resp == 1)
+                {
+                    b = i;
+                    break;
+                }
+            }
+            std::cout << "! " << a + b;
+            break;
+        }
+    }
+}
