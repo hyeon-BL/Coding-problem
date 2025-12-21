2a9890d76f1ad6ccadd12c6b10e180cb35e6f39d
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-12-21 09:58:30 +0900

    Queue - basic application &&  organize folders

---
+++ Data Structure/Queue/카드2.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+queue = deque()
+
+for i in range(1, N+1):
+    queue.append(i)
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue[0])
